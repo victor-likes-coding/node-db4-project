285ea0c74001d8ae881c14f2a73880b9537aea3a
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-26 04:50:06 -0400

    update port as 5000 was being used

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { server } = require('./api');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
