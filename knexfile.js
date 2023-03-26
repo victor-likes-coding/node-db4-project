2a9c94989b1f4de10160b226a36a42a112829e73
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-26 02:50:56 -0400

    create knex file

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3',
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seed: {
+      directory: './seeds',
+    },
+    afterCreate: (conn, cb) => {
+      conn.run('PRAGMA foreign_keys = ON', cb);
+    },
+  },
+};
