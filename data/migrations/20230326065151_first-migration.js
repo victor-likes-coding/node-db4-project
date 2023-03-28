6e0edf68cef8426c95d208c92f5cd1e913cdd4af
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-27 21:39:52 -0400

    update migration

---
+++ data/migrations/20230326065151_first-migration.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema
+    .createTable('recipes', (table) => {
+      table.increments('id');
+      table.string('name', 128).notNullable().unique();
+      table.timestamp('createdAt').defaultTo(knex.fn.now());
+    })
+    .createTable('ingredients', (table) => {
+      table.increments('id');
+      table.string('name', 128).notNullable().unique();
+      table.float('quantity').notNullable();
+    })
+    .createTable('steps', (table) => {
+      table.increments('id');
+      table.integer('step_number').notNullable();
+      table.string('instruction', 1024).notNullable();
+      table.integer('recipe_id').unsigned().notNullable().references('id').inTable('recipes').onDelete('RESTRICT').onUpdate('RESTRICT');
+    })
+    .createTable('ingredients_steps', (table) => {
+      table.increments('id');
+      table.integer('ingredient_id').unsigned().notNullable().references('id').inTable('ingredients').onDelete('RESTRICT').onUpdate('RESTRICT');
+      table.integer('step_id').unsigned().notNullable().references('id').inTable('steps').onDelete('RESTRICT').onUpdate('RESTRICT');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('ingredients_steps').dropTableIfExists('ingredients').dropTableIfExists('steps').dropTableIfExists('recipes');
+};
